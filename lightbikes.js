1fcbf0b84e6341602d30efcf216adbd83a4cbae0
laurenkl96 <lklynch@ksu.edu>
2016-08-26 14:56:43 -0500

    Update lightbikes.js

--- lightbikes.js
+++ lightbikes.js
@@ -1,9 +1,17 @@
 var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
-
+var speed = 1/16/1000;
 var x = 0;
 var y = 0;
-while(true){
-  ctx,fillRect(50,50,50,50);
+canvas.onkeydown = function(event){
+  console.log(event.keyCode);
+  switch(event.keyCode){
+    //case
+  }
 }
-
+function loop(){
+	ctx,fillRect(x,y,50,50);
+	x++;
+	setTimeout(loop, speed);
+}
+  loop();
